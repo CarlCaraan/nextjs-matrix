6a59c8e2016a71ada3f5082cdd24f307e647bef3
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-06-14 20:59:36 +0800

    6/14/23

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -35,6 +35,45 @@ function Footer() {
           </div>
         </div>
         {/* End Flex Container */}
+
+        {/* Socket HR */}
+        <hr className="mt-4" />
+
+        {/* Start Flex Container */}
+        <div className="flex justify-between mt-4">
+          <div className="basis-1/3">
+            Matrix focus on responsive design, user experience, and the latest
+            technologies, we strive to deliver exceptional results that help our
+            clients succeed in the digital world.
+          </div>
+          {/* Footer Links */}
+          <div className="base-1/3 text-panel">
+            <div className="flex space-x-4">
+              <Link className="underline hover:no-underline" href="/">
+                About
+              </Link>
+              <Link className="underline hover:no-underline" href="/">
+                Support
+              </Link>
+              <Link className="underline hover:no-underline" href="/">
+                Contact
+              </Link>
+              <Link className="underline hover:no-underline" href="/">
+                Terms of use
+              </Link>
+              <Link className="underline hover:no-underline" href="/">
+                Privacy
+              </Link>
+            </div>
+          </div>
+        </div>
+        {/* End Flex Container */}
+
+        <p className="text-panel text-sm mt-4 float-right font-sansMedium">
+          Copyright ©{new Date().getFullYear()}{" "}
+          <span className="gradientText">Matrix Software Solution</span> All
+          Rights Reserved
+        </p>
       </div>
     </footer>
   );
