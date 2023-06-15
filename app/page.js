235b380221a8cb5b0af04b83785570c6db62c751
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-06-15 21:12:49 +0800

    herosection responsive

--- app/page.js
+++ app/page.js
@@ -35,20 +35,43 @@ export default function Home() {
       <main>
         {/* ========= Start Herosection ========= */}
         <section className="section-container" id="herosection">
-          <div className="grid grid-cols-12 gap-4 py-28">
+          <div
+            className="grid grid-cols-12 gap-4 py-12 
+            lg:py-16 xl:py-28"
+          >
+            {/* ========= Herosection Image for mobile ========= */}
+            <Image
+              className="block gradientBorder border-b-2 border-r-2 border-t col-span-6 pt-2 pb-6 pr-6 mb-4 shadow-lg
+              xl:hidden sm:col-span-5 md:col-span-4 md:mb-6"
+              src={heroBG}
+              alt="hero-bg"
+            />
+
             {/* Start Left Part */}
-            <div className="col-span-5">
-              <h1 className="text-[2.4vw]">
+            <div
+              className="col-span-12
+              xl:col-span-5"
+            >
+              <h1
+                className="text-lg font-sansMedium w-full 
+                sm:w-2/3 sm:font-sans sm:text-3xl lg:w-full md:text-3xl xl:text-[2.4vw] xl:leading-normal"
+              >
                 We design, develop and build software solution for you
               </h1>
 
-              <h4 className="text-2xl text-soft mt-12">
+              <h4
+                className="text-base text-soft mt-4
+                sm:text-xl 2xl:mt-12 xl:text-2xl"
+              >
                 Build your business with confidence, transform your online
                 vision into reality
               </h4>
 
               {/* Button */}
-              <button className="flex space-x-2 items-center mt-12 border border-dark px-4 py-2 font-sansMedium group hover:shadow-md">
+              <button
+                className="flex space-x-2 items-center mt-4 border border-dark px-4 py-2 font-sansMedium group hover:shadow-md
+                2xl:mt-12"
+              >
                 <div>How IT Works</div>
                 <Image
                   className="w-2 group-hover:ml-4 duration-200"
@@ -57,7 +80,10 @@ export default function Home() {
                 />
               </button>
 
-              <p className="text-soft mt-12">
+              <p
+                className="text-soft mt-4 w-full 
+                lg:w-2/3 2xl:mt-12 xl:w-full"
+              >
                 Matrix is a dynamic, innovative, and forward-thinking start-up
                 company based in <span className="font-sansMedium">Laguna</span>{" "}
                 that takes great pride in providing top-notch, cutting-edge, and
@@ -66,20 +92,32 @@ export default function Home() {
               </p>
 
               {/* Start Flex Container */}
-              <div className="flex gap-[2px] mt-24">
-                <div className="bg-card px-6 pt-4 pb-14 basis-1/3 hover:bg-panel duration-200">
+              <div
+                className="flex flex-col gap-[2px] mt-4
+                sm:mt-12 2xl:mt-24 sm:flex-row"
+              >
+                <div
+                  className="bg-card px-6 pt-4 pb-8 basis-1/3 hover:bg-panel duration-200
+                  2xl:pb-14"
+                >
                   <div className="text-xs text-soft">Expert & Professional</div>
                   <div className="text-sm text-dark font-sansMedium">
                     Experienced Team
                   </div>
                 </div>
-                <div className="bg-card px-6 pt-4 pb-14 basis-1/3 hover:bg-panel duration-200">
+                <div
+                  className="bg-card px-6 pt-4 pb-8 basis-1/3 hover:bg-panel duration-200
+                  2xl:pb-14"
+                >
                   <div className="text-xs text-soft">Trusted Services</div>
                   <div className="text-sm text-dark font-sansMedium">
                     Quality and Efficiency
                   </div>
                 </div>
-                <div className="bg-card px-6 pt-4 pb-14 basis-1/3 hover:bg-panel duration-200">
+                <div
+                  className="bg-card px-6 pt-4 pb-8 basis-1/3 hover:bg-panel duration-200
+                  2xl:pb-14"
+                >
                   <div className="text-xs text-soft">Creative Minds</div>
                   <div className="text-sm text-dark font-sansMedium">
                     Elegant & User-Friendly
@@ -91,13 +129,16 @@ export default function Home() {
             {/* End Left Part */}
 
             {/* Start Right Part */}
-            <div className="col-span-7 relative">
+            <div
+              className="hidden col-span-7 relative 
+              xl:block"
+            >
               <Image
                 className="w-5/6 absolute bottom-0"
                 src={heroBG}
                 alt="hero-bg"
               />
-              <div className="gradientBorder border-4 w-3/5 h-full right-0 -bottom-16 absolute -z-10"></div>
+              <div className="gradientBorder border-4 w-3/5 h-full right-0 -bottom-12 2xl:-bottom-16 absolute -z-10"></div>
             </div>
             {/* End Right Part */}
           </div>
