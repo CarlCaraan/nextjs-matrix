97a238daf82042a3d049ec8b1dbabd938f76c04d
Carl Caraan <bannedefused@gmail.com>
2023-06-18 21:51:56 +0800

    6/18/23

--- app/products/page.js
+++ app/products/page.js
@@ -1,13 +1,36 @@
 // Components
+import Footer from "@/components/layout/Footer";
 import Navbar from "@/components/layout/Navbar";
 
 export default function Products() {
   return (
     <>
+      {/* ========= Start Navigation ========= */}
       <Navbar />
-      <h1 className="text-3xl font-bold text-blue-400 text-center pt-24">
-        Products
-      </h1>
+      {/* ========= End Navigation ========= */}
+
+      {/* ========= Start Main Content ========= */}
+      <main>
+        {/* ========= Start Carousel Section ========= */}
+        <section className="section-container" id="carousel">
+          <h1 className="text-5xl mt-24">Discover our works</h1>
+          <p className="text-xl mt-12 w-1/2 text-soft">
+            See how our expertise in web development has helped businesses like
+            yours achieve online success through innovative and customized
+            solutions
+          </p>
+
+          {/* Start Carousel */}
+          <div></div>
+          {/* End Carousel */}
+        </section>
+        {/* ========= End Carousel Section ========= */}
+      </main>
+      {/* ========= End Main Content ========= */}
+
+      {/* ========= Start Footer ========= */}
+      <Footer />
+      {/* ========= End Footer ========= */}
     </>
   );
 }
