b7f02cdee0ac0df7935f3a6ad4dc48262c727513
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-06-25 20:50:01 +0800

    6/25/23

--- app/solutions/page.js
+++ app/solutions/page.js
@@ -19,11 +19,12 @@ export default function Solutions() {
       <main>
         {/* ========= Start Hero Section ========= */}
         <section
-          className="bg-slate-500 2xl:h-[30vw] overflow-hidden flex items-center relative"
+          className="bg-slate-500 overflow-hidden flex items-center relative h-[50vh]
+          2xl:h-[30vw] sm:h-screen"
           id="herosection"
         >
           <Image
-            className="absolute bg-no-repeat bg-center"
+            className="absolute bg-no-repeat bg-center top-0 w-full h-full object-cover"
             src={solutionsBG}
             alt="solutions-bg"
           />
@@ -31,9 +32,13 @@ export default function Solutions() {
           <div className="absolute inset-0 bg-teal-700 opacity-30"></div>
 
           {/* Caption Text */}
-          <div className="absolute section-container inset-0 flex flex-col justify-center text-white gap-12">
+          <div
+            className="absolute section-container inset-0 flex flex-col justify-center text-white gap-4 
+            sm:gap-6 lg:gap-8 xl:gap-12"
+          >
             <h1
-              className="text-5xl w-2/3 font-sansBold"
+              className="w-2/3 font-sansBold text-lg
+              sm:text-2xl md:text-3xl xl:text-5xl sm:w-5/6 lg:w-2/3"
               style={{
                 textShadow: "2px 2px 4px rgba(0, 0, 0, 0.2)",
               }}
@@ -41,7 +46,8 @@ export default function Solutions() {
               Crafting your Digital Identity with stunning Websites and Logos
             </h1>
             <p
-              className="text-xl w-4/5"
+              className="text-sm w-full
+              xl:text-xl lg:text-lg md:w-4/5"
               style={{
                 textShadow: "2px 2px 4px rgba(0, 0, 0, 0.2)",
               }}
