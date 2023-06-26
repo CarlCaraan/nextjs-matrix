f1239bb635722f3904b627ee77590b84ff0c3d23
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-06-26 14:01:06 +0800

    6/26/23

--- app/solutions/page.js
+++ app/solutions/page.js
@@ -10,6 +10,9 @@ import solutionsBG from "../../public/images/solutions/solutions-bg.jpg";
 
 // SVG
 import calendarIcon from "../../public/images/solutions/calendar-icon.svg";
+import startupIcon from "../../public/images/solutions/startup-icon.svg";
+import standardIcon from "../../public/images/solutions/standard-icon.svg";
+import enterpriseIcon from "../../public/images/solutions/enterprise-icon.svg";
 
 export default function Solutions() {
   return (
@@ -78,6 +81,57 @@ export default function Solutions() {
           </div>
         </section>
         {/* ========= End Hero Section ========= */}
+
+        {/* ========= Start Three Cards Section ========= */}
+        <section className="section-container py-24" id="three-cards-section">
+          {/* Start Flex Container */}
+          <div className="flex space-x-4">
+            <div className="bg-card w-full px-24 py-12 drop-shadow rounded-sm flex flex-col items-center">
+              <Image
+                className="w-24 mt-5"
+                src={startupIcon}
+                alt={startupIcon}
+              />
+              <h4 className="font-sansBold text-2xl mt-[22.5px]">CMS</h4>
+              <h5 className="text-xl text-[#ACACAC]">Starter</h5>
+              <p className="text-justify text-soft mt-2">
+                If you own a small or startup business, it would be beneficial
+                for you to invest in a professional web design for your company,
+                as it has the potential to generate profitable outcomes.
+              </p>
+            </div>
+            <div className="bg-card w-full px-24 py-12 drop-shadow rounded-sm flex flex-col gradientBorder border-2 items-center">
+              <Image className="w-32" src={standardIcon} alt={standardIcon} />
+              <h4 className="font-sansBold text-2xl mt-2">Small-Scale</h4>
+              <h5 className="text-xl text-[#ACACAC]">Standard</h5>{" "}
+              <p className="text-justify text-soft mt-2">
+                This option is ideal for individuals and businesses aiming to
+                enter the online market and commence their sales activities on
+                the internet.
+              </p>
+            </div>
+            <div className="bg-card w-full px-24 py-12 drop-shadow rounded-sm flex flex-col items-center">
+              <Image
+                className="w-24 mt-4"
+                src={enterpriseIcon}
+                alt={enterpriseIcon}
+              />
+              <h4 className="font-sansBold text-2xl mt-[24px]">
+                Enterprise Website
+              </h4>
+              <h5 className="text-xl text-[#ACACAC]">Plus</h5>
+              <p className="text-justify text-soft mt-2">
+                This type of website offers tailored features with custom
+                functions designed to fit your specific needs. Investing a
+                significant amount in a website like this may seem like a
+                substantial financial commitment, but you can be confident that
+                you will receive the full value of what you paid for.
+              </p>
+            </div>
+          </div>
+          {/* End Flex Container */}
+        </section>
+        {/* ========= End Three Cards Section ========= */}
       </main>
       {/* ========= End Main Content ========= */}
 
