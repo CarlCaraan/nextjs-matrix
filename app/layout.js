8f6b1f43cde198c04d6b15022e8b71b45273b7be
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-07-13 10:54:26 +0800

    7/13/23

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,18 @@
+import Head from "next/head";
+
 // Styles
 import "./globals.css";
+
+// Icon
+import favicon from "../public/favicon.ico";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        {/* Icon */}
+        <link rel="icon" type="icon" href={favicon}></link>
+      </Head>
       <body className="relative antialiased font-sans text-dark">
         {children}
       </body>
